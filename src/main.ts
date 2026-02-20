af900657b2d5504a3b083d8d0f67ea990a447189
j-camposar <31163773+j-camposar@users.noreply.github.com>
2026-02-19 22:47:52 -0300

    fix deploy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
    app.enableCors({
-    origin: ['http://localhost:3000', "http://192.168.1.132:3000"],
+    origin: ['http://localhost:3000',"http://192.168.1.132:3000", "https://gym-rpg-front-fh6r.vercel.app"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
