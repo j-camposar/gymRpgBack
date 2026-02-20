6afc731d175e433e1456fe2f3c5b1e6f78a4be6f
j-camposar <31163773+j-camposar@users.noreply.github.com>
2026-02-19 23:49:17 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-   app.enableCors({
-    origin: ['http://localhost:3000',"http://192.168.1.132:3000", "https://gym-rpg-front-fh6r.vercel.app"],
+
+  // Configuración de CORS
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://192.168.1.132:3000', 
+      'https://gym-rpg-front-fh6r.vercel.app' // Tu URL de Vercel
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
-  await app.listen(process.env.PORT ?? 3001, '0.0.0.0',() => {
-    });
+
+  // Render asigna el puerto mediante la variable PORT
+  const port = process.env.PORT || 3001;
+
+  // Escuchar en '0.0.0.0' es fundamental para Docker/Render/Railway
+  await app.listen(port, '0.0.0.0');
+  
+  logger.log(`El Arca está navegando en el puerto: ${port}`);
 }
 bootstrap();
